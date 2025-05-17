f137feac4617415759e0d7c370de17fe3793ee5f
CarlosBF <cborrellf@outlook.es>
2025-05-18 00:38:21 +0200

    Finnished. Test passed, might need improvements

---
+++ product.js
@@ -0,0 +1,68 @@
+let products = []
+let id = 0
+
+function resetProducts() {
+    products = []
+    id = 0
+}
+
+function addProduct(name, price) {
+    if(name === undefined || price === undefined){
+        throw new Error('name and price must be defined')
+    }else if(products.findIndex(product => product.name === name) != -1){
+        throw new Error('Product with that name already exists')
+    }else{
+        const product = {
+            id: id,
+            name: name,
+            price: price
+        }
+        products.push(product)
+        id++
+    }
+}
+
+function removeProduct(id){
+    const index = products.findIndex(prod => prod.id === id)
+    if(index === -1){
+        throw new Error('Product does not exist')
+    }else{
+        products.splice(index, 1)
+    }
+}
+
+function getProducts() {
+    return products
+}
+
+function getProduct(id) {
+    const product = products.find(prod => prod.id === id)
+    if(product === undefined){
+        throw new Error('Product does not exist')
+    }else{
+        return product
+    }
+}
+
+function updateProduct(id, name, price) {
+    const index = products.findIndex(product => product.id === id)
+    if(index === -1){
+        throw new Error('Product does not exist')
+    }else{
+        if(name !== undefined){
+            products[index].name = name
+        }
+        if(price !== undefined){
+            products[index].price = price
+        }
+    }
+}
+
+module.exports = {
+    resetProducts,
+    addProduct,
+    removeProduct,
+    getProducts,
+    getProduct,
+    updateProduct
+}
---
+++ product.test.js
@@ -0,0 +1,49 @@
+const { resetProducts,  addProduct, removeProduct, getProducts, getProduct, updateProduct} = require('./product.js');
+
+beforeEach(() => {
+    resetProducts();
+});
+
+describe('addProduct', () => {
+    it('should add a product and increase the id', () => {
+        expect(addProduct('Product1', 1))
+    });
+    it('should throw error if product already exists', () => {
+        addProduct('Product1', 1)
+        expect(() => addProduct('Product1', 1)).toThrow('Product with that name already exists');
+    });
+    it('should throw error if product params are not defined', () => {
+        expect(() => addProduct('Product1')).toThrow('name and price must be defined');
+    });
+});
+
+describe('removeProduct', () => {
+    it('should remove a product given the id', () => {
+        addProduct('Product1', 1)
+        expect(removeProduct(0))
+    });
+    it('should throw error if product does not exist', () => {
+        expect(() => removeProduct(0)).toThrow('Product does not exist')
+    });
+});
+
+describe('getProduct', () => {
+    it('should return a product given the id', () => {
+        addProduct('Product1', 1)
+        expect(getProduct(0)).toEqual({ id: 0, name: 'Product1', price: 1 })
+    });
+    it('should throw error if product does not exist', () => {
+        expect(() => getProduct(0)).toThrow('Product does not exist')
+    });
+});
+
+describe('updateProduct', () => {
+    it('should update an existing product with the provided parameters', () => {
+        addProduct('Product1', 1)
+        expect(updateProduct(0, 'Updated', 1.5))
+    });
+    it('should return error if product does not exist', () => {
+        expect(() => updateProduct(0, 'Updated', 1.5)).toThrow('Product does not exist')
+    });
+});
+
